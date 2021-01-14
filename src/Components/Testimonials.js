93bda9024c19cacef86a73a05d5568371e6774ba
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-01-14 22:20:25 +0100

    refactor: comented icon Testimonials in `Testimonials.js`component

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -19,9 +19,9 @@ class Testimonials extends Component {
       <div className="text-container">
          <div className="row">
 
-            <div className="two columns header-col">
+            {/* <div className="two columns header-col">
                <h1><span>Client Testimonials</span></h1>
-            </div>
+            </div> */}
 
             <div className="ten columns flex-container">
                   <ul className="slides">
